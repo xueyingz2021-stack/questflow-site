242fa6c1a4dd6a803a5c7619d638c036fdf77c50
xueyingz2021-stack <xueyingz2021@gmail.com>
2026-02-10 16:53:40 +0800

    Add files via upload

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,9 +1,8 @@
 "use client";
 
-import Link from "next/link";
-import { motion } from "framer-motion";
 import Navbar from "@/components/home/Navbar";
 import HeroSection from "@/components/home/HeroSection";
+import SponsorMarquee from "@/components/home/SponsorMarquee";
 import HowItWorks from "@/components/home/HowItWorks";
 import CloneAnatomy from "@/components/home/CloneAnatomy";
 import RevenueStreams from "@/components/home/RevenueStreams";
@@ -17,6 +16,7 @@ export default function HomePage() {
     <div className="min-h-screen bg-[var(--color-surface-primary)] text-white overflow-x-hidden">
       <Navbar />
       <HeroSection />
+      <SponsorMarquee />
       <HowItWorks />
       <CloneAnatomy />
       <RevenueStreams />
